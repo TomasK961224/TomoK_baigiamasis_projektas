dfae365063f61d688714e68bec5a2d2d1a712b5a
TomasK961224 <tomaskoncevojus@gmail.com>
2024-08-17 18:47:54 +0300

    Delete TomoK_baigiamasis_projektas directory

--- TomoK_baigiamasis_projektas/TomoK_baigiamasis_projektas/animals/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('categories/', views.animal_categories, name='animal_categories'),
-]
